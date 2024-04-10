af9d9db11102badfd7c0676434de770513b55e80
Blizzarddz <162144023+Blizzarddz@users.noreply.github.com>
2024-04-10 13:56:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function changeGif(){
+    document.getElementById('gifImage').src="gengar.gif";
+    document.getElementById('responseText').style.display = 'block';
+    document.getElementById('yesButton').style.display ='none';
+    document.getElementById('marryMeText').style.display = 'none';
+    document.getElementById('Noresponse').style.display ='none';
+}
